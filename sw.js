4625a9b7d25617e8356c27a2a0e8740477a1f0e4
creichmd-code <creichmd@gmail.com>
2025-10-05 15:02:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request))});
+const CACHE_NAME = 'bujo-cache-v5'; // bump to avoid stale cache
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
